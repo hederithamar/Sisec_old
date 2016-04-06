2a273f5c97a12de0e55105176413596a1558e2c6
hederithamar <heder.ithamar.hr@gmail.com>
2016-04-06 16:56:35 +0000

    Creando Seeder y modificndo Migraciones

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //factory(App\User::class, 49)-> create();
+        /*factory(App\User::class)-> create([
+        	'name' 			=>'Heder',
+        	'firstlastname' => 'Romero',
+        	'secondlastname' => 'Romero',
+        	'email' 		=> 'admin@hotmail.com',
+        	'state' 		=> 'Puebla',
+        	'zone' 			=> 'Sur',
+        	'celphone' 		=> '123456789',
+        	'phone' 		=> '123456678',
+        	'role_id' 		=> '1',
+        	'enterprice_id' => '1',
+        	'id_curp' 		=> 'HEDERiTHAMARROMERO'
+        	]);*/
+        	
+    }
+}
