4ec8b77ba20423deed70ab4569bcb68d838c99d6
Francisco <paco_games@hotmail.com>
2016-04-11 15:53:28 +0000

    Prueba estilos

--- resources/views/empresa/forms/emp.blade.php
+++ resources/views/empresa/forms/emp.blade.php
@@ -2,7 +2,7 @@
 	<div class="col-md-6 form-group">
 		{!!Form::label('enterprice_id','Escribe el RFC:')!!}
 		<br>
-		{!!Form::select('rfc1', $enterprices, null, ['id'=>'rfc1','class' => 'js-example-tags js-states form-control']);!!}
+		{!!Form::select('rfc', $enterprices, null, ['id'=>'rfc1','class' => 'js-example-tags js-states form-control']);!!}
 	</div>
 
 	<div class="col-md-6 form-group">
